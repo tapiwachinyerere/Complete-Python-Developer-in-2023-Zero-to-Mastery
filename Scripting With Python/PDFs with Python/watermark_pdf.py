b58cee66b4a03e4875616f8059c704508a284c0a
dzvinyangwi <editorofcode@gmail.com>
2023-12-15 04:23:35 +0100

    docs: :memo: Docstring
    updated docstring of watermark_pdf

--- Scripting With Python/PDFs with Python/watermark_pdf.py
+++ Scripting With Python/PDFs with Python/watermark_pdf.py
@@ -1,6 +1,20 @@
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
 def watermark_pdf(watermark, file, new_file):
+    """
+    Adds a watermark to each page of the specified PDF file.
+
+    Args:
+    - watermark (str): The file path of the PDF containing the watermark.
+    - file (str): The file path of the PDF to be watermarked.
+    - new_file (str): The file path where the resulting watermarked PDF will be saved.
+
+    Returns:
+    - None
+
+    Note:
+    - The function overlays the watermark PDF on each page of the input PDF file and saves the result as a new PDF.
+    """
     stamp = PdfFileReader(f'{watermark}')
     reader = PdfFileReader(f'{file}')
     writer = PdfFileWriter()
